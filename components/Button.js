e128be944d2e60af66b304767a70839f055bafe1
maishio <masanori.ishio119@gmail.com>
2023-05-29 10:02:32 +0900

    refactor: modify

--- components/Button.js
+++ components/Button.js
@@ -1,16 +1,24 @@
+import { primary } from '@/lib/color'
 import styled, { css } from 'styled-components'
 
-const StyledButton = styled.button`
+export const ButtonStyle = css`
   border: 0;
   padding: 5px 15px;
   border-radius: 5px;
   cursor: pointer;
   display: inline-flex;
   align-items: center;
+  text-decoration: none;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
   svg{
     height: 16px;
     margin-right: 5px;
   }
+  ${props => props.block && css`
+    display: block;
+    width: 100%;
+  `}
   ${props => props.white && !props.outline && css`
     background-color: #fff;
     color: #000;
@@ -20,10 +28,24 @@ const StyledButton = styled.button`
     color: #fff;
     border: 1px solid #fff;
   `}
-  ${props => props.primary && css`
-    background-color: #5542F6;
-    border: 1px solid #5542F6;
+  ${props => props.black && !props.outline && css`
+    background-color: #000;
     color: #fff;
+  `}
+  ${props => props.black && props.outline && css`
+    background-color: transparent;
+    color: #000;
+    border: 1px solid #000;
+  `}
+  ${props => props.primary && !props.outline && css`
+    background-color: ${primary};
+    border: 1px solid ${primary};
+    color: #fff;
+  `}
+  ${props => props.primary && props.outline && css`
+    background-color: transparent;
+    border: 1px solid ${primary};
+    color: ${primary};
   `}
   ${props => props.size === 'l' && css`
     font-size: 1.2rem;
@@ -32,6 +54,10 @@ const StyledButton = styled.button`
       height: 20px;
     }
   `}
+`
+
+const StyledButton = styled.button`
+  ${ButtonStyle}
 `
 
 export default function Button({children, ...rest}) {
